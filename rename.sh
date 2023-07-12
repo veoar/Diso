efa1abe4962bcb8111bfe00fc2f57593b8641de9
veoar <118748077+veoar@users.noreply.github.com>
2023-07-12 19:29:08 +0100

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This renames the summary file to have the time ID code
+parent_directory="/home/mbxjt11/scratch/CRISPRCasFinder-master"
+
+for directory in "$parent_directory"/Result_GCF*; do
+    if [ -d "$directory" ]; then
+        directory_id=$(basename "$directory" | awk -F '_' '{print $NF}')
+        tsv_directory="$directory/TSV"
+        for filename in "$tsv_directory"/*; do
+            new_filename=$(basename "$filename" | sed -e "s/REPORT/REPORT_$directory_id/" -e "s/summary/summary_$directory_id/")
+            mv "$filename" "$tsv_directory/$new_filename"
+        done
+    fi
+done
