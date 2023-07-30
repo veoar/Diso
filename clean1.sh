d576756c1ea6d97f727b1216f4bcb2327bef84be
Jordan <118748077+veoar@users.noreply.github.com>
2023-07-30 01:43:04 +0100

    Create clean1.sh

---
+++ clean1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Set the source and destination directories
+source_dir="/home/mbxjt11/scratch/Prokka_results"
+dest_dir="/home/mbxjt11/scratch/prokka_gff"
+
+# Create the destination directory if it doesn't exist
+mkdir -p $dest_dir
+
+# Move all .fna files from the source directory and its subdirectories to the destination directory
+find $source_dir -name "*.gff" -exec mv {} $dest_dir \;
