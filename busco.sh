a1a3b874232f2f72bddf15e459410ee5500b5217
Jordan <118748077+veoar@users.noreply.github.com>
2023-07-30 01:23:17 +0100

    Create busco.sh

---
+++ busco.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+--cpus [N]         Number of CPUs to use [0=all]
+
+# Set the paths
+input_dir="/home/mbxjt11/scratch/clean"
+lineage_path="/home/mbxjt11/scratch/busco_downloads/lineages/alphaproteobacteria_odb10"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Iterate over the .fna files in the input directory
+for file in "$input_dir"/*.fna; do
+    # Get the filename without extension
+    filename=$(basename "$file" .fna)
+
+    # Run BUSCO QC
+    busco -i "$file" -l "$lineage_path" -m genome -o Busco_QC
+done
+
